f00ca58821e9ce7891721e18b062406ad2aa8bf6
Joseph <joeseffdl@gmail.com>
2021-11-16 02:49:57 +0800

    Index Page
    Draft for Index Page -> Dev Page

---
+++ Clinic-Management-System/src/layouts/MainLayout.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header reveal elevated>
+      <q-toolbar class="glossy bg-primary">
+        
+        <!-- Nav Bar -->
+        <q-toolbar-title style="padding: 0.25em">
+            <q-img src="icons\code.png" alt="company logo"
+          style="width:2.5em;height:2.5em;" /> Best <span class="text-accent ">Byte</span>
+        </q-toolbar-title>
+
+        <div class="gt-sm">
+          <q-btn outline rounded class="text-accent" size="1.25em" label="Dashboard"></q-btn>
+        </div>
+
+        <!-- Start Dashboard -->
+        <div class="lt-md">
+          <q-btn
+            flat
+            dense
+            round
+            class="text-accent"
+            icon="mdi-menu"
+            aria-label="Menu"
+            size="1.75em"
+            @click="toggleLeftDrawer"
+          />
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Clinic Dashboard
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+    <!-- End Dashboard -->
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <q-footer reveal elevated >
+        <q-toolbar class="glossy bg-primary">
+          <q-space/>&copy; Snow Squirrel 2021<q-space/>
+        </q-toolbar>
+      </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'About Us',
+    caption: 'Company Profile',
+    icon: 'mdi-store',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Help',
+    caption: 'System Guide',
+    icon: 'mdi-help',
+    link: 'https://quasar.dev'
+  },
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ Clinic-Management-System/src/pages/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page class="bg-secondary ">
+    <div class="row text-center">
+      <div class="col-6 gt-md self-center q-mt-xl" >
+        <q-img src="icons\code.png" style="width:600px;height:600px;"></q-img>
+      </div>
+      <div class="col-6 q-gutter-xl self-center " style="max-width:650px;margin: 0 auto 0 auto;" >
+        <div class="text-h2 ">Login as:</div>
+        <q-btn
+          outline
+          style="color: goldenrod"
+          class=""
+          icon="mdi-account"
+          label="Guest"
+          size="3em"
+        />
+        <q-btn
+          outline
+          style="color: black"
+          class=""
+          icon="mdi-code-tags"
+          label="Admin"
+          size="3em"
+        />
+        <div class="text-h6 q-mb-lg">New here? Sign Up now!</div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+
+  data() {
+    return {
+      selected: null,
+    };
+  },
+});
+</script>
