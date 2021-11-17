88e3d31f40d98d349feaf63523e8f8b8c2cac7ae
ApplePineappleee <blancheann8@gmail.com>
2021-11-17 16:21:27 +0800

    Update Signup.vue

--- Clinic-Management-System/src/pages/Signup.vue
+++ Clinic-Management-System/src/pages/Signup.vue
@@ -2,7 +2,7 @@
   <q-page class="bg-secondary ">
     <div class="row fullscreen text-center items-center">
       <div class="col-6 gt-md self-center">
-        <div class="col self-center text-h1 " style="margin: 0 auto 0 auto;">Health is Wealth</div>
+        <div class="col self-center text-h1 " style="margin: 0 auto 0 auto;">Best Bite</div>
       </div>
       <div class="col-6 " style="width:750px;margin: 0 auto 0 auto;" >
         
