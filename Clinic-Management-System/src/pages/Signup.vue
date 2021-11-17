daf93de38002af5b569fca74358cd9e9f1fbdf22
Joseph <joeseffdl@gmail.com>
2021-11-17 20:04:43 +0800

    Form Draft

--- Clinic-Management-System/src/pages/Signup.vue
+++ Clinic-Management-System/src/pages/Signup.vue
@@ -5,7 +5,16 @@
         <div class="col self-center text-h1 " style="margin: 0 auto 0 auto;">Quote of the day</div>
       </div>
       <div class="col-6 " style="width:750px;margin: 0 auto 0 auto;" >
-        
+        <q-form @submit="submitForm">
+          <div>
+            <q-input  v-model="formData.firstName" square filled dense color="primary" 
+            label="First Name" clearable clear-icon="mdi-close"
+            :rules="[val => !!val || 'Field is required!']" />
+          </div>
+          <div class="q-pt-lg">
+            <q-btn pushed type="submit" label="Submit" />
+          </div>
+        </q-form>
       </div>
     </div>
   </q-page>
@@ -15,12 +24,19 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "PageIndex",
+  name: "PageSignup",
 
   data() {
     return {
-      selected: null,
-    };
+      formData: {
+        firstName: null,
+        }
+    }
   },
+  methods: {
+    submitForm() {
+      alert('Your name is: ' + this.formData.firstName);
+    }
+  }
 });
 </script>
