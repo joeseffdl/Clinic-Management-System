60da99695d92285d6f6b20f3267d130a3e2ac755
agguillermo <gguillermoantoinette@gmail.com>
2021-11-24 19:27:57 +0800

    Profile Settings Layout and Page, Error Solution in Eslint, and Profile Settings Path in Routes

---
+++ Clinic-Management-System/src/layouts/ProfileSettingsLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="bg-secondary">
+    <q-header reveal elevated>
+      <q-toolbar class="row glossy bg-primary items-center justify-center">
+        <div class="col">
+            <q-btn outline rounded style="width: 105px" color="secondary" label="Back" icon="mdi-arrow-left"/>
+        </div>
+        <q-toolbar-title class="col" style="padding: .25em;">
+            Settings
+        </q-toolbar-title>
+        <div class="text-h5"> Best<span class="text-accent ">Byte</span> <q-img src="icons\logo_white.png" alt="company logo"
+          style="width:2em;height:2em;"/>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer reveal elevated >
+        <q-toolbar class="glossy bg-primary">
+          <q-space/>&copy; Snow Squirrel 2021<q-space/>
+        </q-toolbar>
+      </q-footer>
+      
+  </q-layout>
+</template>
