c812066e3dc2f98b5df29f1e84eeff61d3a1f91f
agguillermo <gguillermoantoinette@gmail.com>
2021-12-03 18:52:57 +0800

    Route changes for About Us page, and About Us Page

---
+++ Clinic-Management-System/src/pages/AboutUs.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-page class="bg-secondary">
+      <div class="column fit q-pa-xl" style="padding-top: 60px">
+        <div class="col-1 fit" style="margin: 0 0 60px 0">
+          <div class="text-h2 text-center" style="">About Us</div>
+        </div>
+        <div class="col-8 fit">
+          <div class="row items-center justify-center" style="margin: 0 auto 0 auto; max-width: 1000px;">
+            <div class="col" style="margin: 0 20px 0 20px;">
+              <div class="column">
+                <div class="col self-center" style="margin: 0 0 -20px 0;">
+                  <div class="text-h6 text-center">Clinic Logo</div>
+                </div>
+
+                <div class="col self-center" style="margin: 0 0 -20px 0; width: 300px">
+                  <q-img class="q-mt-md" src="icons\byte_black.png" spinner-color="black" style="height:300px; max-width: 300px;"/>
+                </div>
+
+                <div class="col self-center">
+                  <div class="text-h5" style="margin: 20px 30px 10px 30px;">Contact Us</div>
+                  <div class="text-h7 rounded-borders q-pa-md" style="background-color: #0E86D4; margin: 0 30px 30px 30px; text-align: justify; color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
+                </div>
+             </div>
+            </div>
+
+            <div class="col gt-sm rounded-borders" style="background-color: #0E86D4; margin: 20px;">
+              <div class="text-h7 q-pa-md" style="text-align: justify; color: white;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis sodales enim, sit amet luctus turpis. Morbi commodo leo id ante facilisis sodales. Sed tempor facilisis ex ac luctus. Nam ultricies leo nisi, et accumsan odio ultricies sed. Aliquam laoreet neque ut nulla dapibus gravida. Quisque non erat ipsum. Morbi dolor nulla, interdum quis quam in, varius iaculis turpis. Aenean aliquam, ex non maximus eleifend, nisi sem ornare purus, quis mattis lectus sapien vel dolor. Suspendisse pharetra ex vel ligula sodales eleifend. Nunc sollicitudin neque condimentum ornare cursus. Phasellus vel convallis mauris, id pretium quam. Phasellus mattis ipsum eu ornare porttitor. Donec a ornare nisl, ultrices vehicula mi. In hac habitasse platea dictumst. Quisque risus erat, rhoncus vel sapien id, sodales aliquet lacus. Aliquam libero urna, lacinia sed placerat nec, varius sed enim. Duis id dignissim ante. Donec sit amet ligula tincidunt, tristique nisl eu, euismod tortor. Curabitur non ultrices orci, varius porttitor sapien. Vestibulum placerat sodales nisi eget cursus. Duis ultricies nisl turpis, vitae luctus massa vulputate in.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </q-page>
+</template>
